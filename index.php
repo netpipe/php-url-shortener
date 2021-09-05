5d86501cf395829b35ebf2a6d93398f2046db451
rolaur <rolaur@ttu.ee>
2021-09-05 23:11:34 +0300

    first iteration

---
+++ index.php
@@ -0,0 +1,71 @@
+<?php
+
+require_once "functions.php";
+
+$slug = "";
+
+if (isset($_SERVER['REQUEST_URI']) && slugMeetsRequirements($_SERVER['REQUEST_URI'])) {
+    $slug = substr(trim($_SERVER['REQUEST_URI'], "/"), 0, SLUG_LEN);
+    goToUrl($slug);
+}
+
+if (isset($_POST['url']) && isset($_POST['password']) && $_POST['password'] === PASS && urlIsCorrect($_POST['url']) && !urlAlreadyShortened($_POST['url'])) {
+    $slug = addUrlToDatabase($_POST['url']);
+    header("Location: " . BASE . "/?slug=" . $slug);
+}
+
+if (isset($_GET['slug'])) {
+    $slug = $_GET['slug'];
+}
+
+?>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.80.0">
+    <title>URL shortener - link.that.ee</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <meta name="theme-color" content="#563d7c">
+
+    <link href="styles.css" rel="stylesheet">
+</head>
+<body>
+
+<form class="form-shorten-url" action="index.php" method="post">
+    <div class="text-center mb-4">
+        <h1 class="h3 mb-3 font-weight-normal"><a href="https://link.that.ee" class="text-reset">URL shortener</a></h1>
+        <p>Shorten urls. Github repo can be found here.</a></p>
+    </div>
+    <div class="form-label-group">
+        <input type="text" id="url"  name="url" class="form-control" placeholder="URL to shorten" required autofocus>
+        <label for="url">URL to shorten</label>
+        <small id="urlHelp" class="form-text text-muted">Needs to begin with https:// or http://</small>
+
+    </div>
+    <div class="form-label-group mb-3">
+        <input type="password" name="password" id="password" class="form-control" placeholder="Password" required>
+        <label for="password">Password</label>
+        <small id="passwordHelp" class="form-text text-muted">
+            If you want to use it then you need password. If you don't have password then probably
+            it would be good to set up your own. See the git repo.
+        </small>
+    </div>
+    <div class="form-label-group mb-3">
+        <?php
+        if (isset($_GET['slug'])) {
+            echo "Just created: <code>" . BASE . "/$slug</code>";
+        }
+        ?>
+    </div>
+
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Create</button>
+
+</form>
+
+</body>
+</html>
---
+++ readme.md
@@ -0,0 +1,8 @@
+## Simple URL shortener 
+
+- Written in php 8, 
+- Using sqlite3 for as data storage. Database creation script included. 
+- Bootstrap 4.6 for css
+
+Before using .htaccess also needs to be configured.  
+Basic [WP configuration](https://wordpress.org/support/article/htaccess/) does the job.
